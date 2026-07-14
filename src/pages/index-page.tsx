d6563a2f4a04167a5341a0ce355a0c1eaef97065
Kimball Jardine <knj28@byu.edu>
2026-07-14 16:31:31 -0600

    Remove antipattern with dangerouslySetInnerHTML.

--- src/pages/index-page.tsx
+++ src/pages/index-page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const indexBodyHtml = `
-<noscript>You need to enable JavaScript to run this app.</noscript>
-<div id="root"></div>
-<script type="module" src="/index.tsx"></script>
-`;
-
 export default function IndexPage() {
-  return <div dangerouslySetInnerHTML={{ __html: indexBodyHtml }} />;
+  return (
+    <>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <div id="root" />
+      <script type="module" src="/index.tsx"></script>
+    </>
+  );
 }
